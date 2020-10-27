222372eabb5b5e76598b9df8fd466e660ce5a9e7
Maksim <maksimus-94@tut.by>
2020-10-27 12:53:58 +0300

    structure main + structure admin + post component

--- blog-angular/src/app/admin/admin.module.ts
+++ blog-angular/src/app/admin/admin.module.ts
@@ -2,22 +2,24 @@ import {NgModule} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterModule} from '@angular/router';
 import {AdminLayoutComponent} from './shared/components/admin-layout/admin-layout.component';
-import { LoginPageComponent } from './login-page/login-page.component';
-import { DashboardPageComponent } from './dashboard-page/dashboard-page.component';
-import { CreatePageComponent } from './create-page/create-page.component';
-import { EditPageComponent } from './edit-page/edit-page.component';
+import {LoginPageComponent} from './login-page/login-page.component';
+import {DashboardPageComponent} from './dashboard-page/dashboard-page.component';
+import {CreatePageComponent} from './create-page/create-page.component';
+import {EditPageComponent} from './edit-page/edit-page.component';
 
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild([
-      {path: '', component: AdminLayoutComponent, children: [
+      {
+        path: '', component: AdminLayoutComponent, children: [
           {path: '', redirectTo: '/admin/login', pathMatch: 'full'},
           {path: 'login', component: LoginPageComponent},
           {path: 'dashboard', component: DashboardPageComponent},
           {path: 'create', component: CreatePageComponent},
           {path: 'post/:id/edit', component: EditPageComponent}
-        ]}
+        ]
+      }
     ])
   ],
   exports: [
