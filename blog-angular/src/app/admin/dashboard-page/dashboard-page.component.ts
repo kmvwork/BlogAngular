b6f520fa1dffe650a662db62c916758a9e097268
Maksim <maksimus-94@tut.by>
2020-11-07 13:23:06 +0300

    output a list of posts

--- blog-angular/src/app/admin/dashboard-page/dashboard-page.component.ts
+++ blog-angular/src/app/admin/dashboard-page/dashboard-page.component.ts
@@ -1,15 +1,33 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {PostsService} from "../../shared/posts.service";
+import {Post} from "../../shared/interfaces";
+import {Subscription} from "rxjs";
 
 @Component({
   selector: 'app-dashboard-page',
   templateUrl: './dashboard-page.component.html',
   styleUrls: ['./dashboard-page.component.scss']
 })
-export class DashboardPageComponent implements OnInit {
+export class DashboardPageComponent implements OnInit, OnDestroy {
+  posts: Post[] = []
+  pSub: Subscription
 
-  constructor() {
+  constructor(private postService: PostsService) {
   }
 
   ngOnInit(): void {
+    this.pSub = this.postService.getAll().subscribe(posts => {
+      this.posts = posts
+    })
+  }
+
+  ngOnDestroy() {
+    if (this.pSub) {
+      this.pSub.unsubscribe()
+    }
+  }
+
+  remove(id: string) {
+
   }
 }
--- blog-angular/src/app/shared/posts.service.ts
+++ blog-angular/src/app/shared/posts.service.ts
@@ -18,12 +18,23 @@ export class PostsService {
       .pipe(
         map((response: FBCreateResponse) => {
           const newPost: Post = {
-            ... post,
+            ...post,
             id: response.name,
             date: new Date(post.date)
           }
           return newPost
         })
       )
+  }
+
+  getAll(): Observable<Post[]> {
+    return this.http.get(`${environment.fbDbUrl}/posts.json`)
+      .pipe(map((response: { [key: string]: any }) => {
+        return Object.keys(response).map(key => ({
+          ...response[key],
+          id: key,
+          date: new Date(response[key].date)
+        }))
+      }))
   }
 }
